588ef383371fa39999b357efdaed9c223b48eb28
jake-mg-shin <60331338+jake-mg-shin@users.noreply.github.com>
2020-03-04 20:57:52 -0700

    Updates

---
+++ precache-manifest.cf85bd75269af9b3b2afebf3a9b13356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b9ed1137d5e872aac05e60baa1e331",
+    "url": "/movie-app-v1/index.html"
+  },
+  {
+    "revision": "a632f1c80d7368c4c2f9",
+    "url": "/movie-app-v1/static/css/main.c6567923.chunk.css"
+  },
+  {
+    "revision": "39d440c0fe3204f8cca9",
+    "url": "/movie-app-v1/static/js/2.51d1794c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie-app-v1/static/js/2.51d1794c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a632f1c80d7368c4c2f9",
+    "url": "/movie-app-v1/static/js/main.5ab48274.chunk.js"
+  },
+  {
+    "revision": "8eb15b568d6db66945ba",
+    "url": "/movie-app-v1/static/js/runtime-main.5661bf0e.js"
+  }
+]);
